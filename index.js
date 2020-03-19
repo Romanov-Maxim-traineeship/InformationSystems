7efa796688c651491c8bbd063ff743217e8cb7b2
Maxim Romanov <maxim.romanov@jetrockets.ru>
2020-03-20 00:39:31 +0300

    Finish fixes

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const { makeArray, fillArray } = require("./readline");
 const bubbleSortArray = require("./bubbleSort");
 
-const arr = fillArray(makeArray());
-
-console.log(
-  bubbleSortArray(arr.filter(item => typeof item === "number" && !isNaN(item)))
-);
+console.log(bubbleSortArray(fillArray(makeArray())));
--- readline.js
+++ readline.js
@@ -1,18 +1,28 @@
 const readline = require("readline-sync");
 
 const makeArray = () => {
-  const width = +readline.question("Размер массива? ");
-  let outputArray = new Array(width);
-  return outputArray;
+  const getArraySize = () => {
+    const width = +readline.question("Размер массива? ");
+    if (isNaN(+width) || +width < 1) return getArraySize();
+    return width;
+  };
+
+  return new Array(getArraySize());
 };
 
 const fillArray = array => {
   console.log("Заполните массив: ");
-  for (let i = 0; i < array.length; i++) {
-    const numberForArray = readline.question(`${i} элемент: `);
 
+  const fillItemOfNumber = i => {
+    const numberForArray = readline.question(`${i} элемент: `);
+    if (isNaN(+numberForArray)) return fillItemOfNumber(i);
     array[i] = +numberForArray;
+  };
+
+  for (let i = 0; i < array.length; i++) {
+    fillItemOfNumber(i);
   }
+
   return array;
 };
 
